bf752e02ef7a3ec2980653fa0754f245ba72c1c1
SOURAV <souravmohanta4428@gmail.com>
2026-01-13 12:54:48 +0530

    removing exta code

--- FRONT_END/src/componets/header.tsx
+++ FRONT_END/src/componets/header.tsx
@@ -1,6 +1,6 @@
 export const Header=()=>{
     return (
-        <div className="flex justify-between p-4 border-b-1 border-gray-200">
+        <div className="flex justify-between p-4 border border-gray-200">
             <div className="pl-24">LOGO</div>
             <div className="flex justify-between space-x-3">
                 <div className="pt-1 text-gray-500">Features</div>
